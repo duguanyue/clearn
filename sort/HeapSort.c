90bd9973c5c97fed826492c2d5be007c656efaed
duguanyue <duguanyue@gmail.com>
2018-10-05 19:09:34 +0800

    heap sort & quick sort

---
+++ sort/HeapSort.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+//使用大根堆
+#define M 7
+int arr[M];
+void Swap(int *a, int *b) {
+    int c = *a;
+    *a = *b;
+    *b = c;
+}
+void Adjust(int arr[], int i, int n) {
+    int j, c = arr[i];
+    while ((j = 2 * i + 1, j) < n) {
+        if (j + 1 < n && arr[j] < arr[j + 1])
+            j++;
+        if (c < arr[j])
+            Swap(arr + i, arr + j);
+        else
+            break;
+        i = j;
+    }
+}
+int main(int argc, char const *argv[]) {
+    srand(time(NULL));
+    int i, t = 6;
+    while (t--) {
+        for (i = 0; i < M; i++) {
+            arr[i] = rand() % 100;
+            printf("%d, ", arr[i]);
+        }
+        puts("\n堆排序(大根堆)-----------------");
+        for (i = M / 2; i >= 0; i--)
+            Adjust(arr, i, M);
+        for (i = M - 1; i > 0; i--) {
+            Swap(arr, arr + i);
+            Adjust(arr, 0, i);
+        }
+        //输出
+        for (i = 0; i < M; i++) {
+            printf("%d, ", arr[i]);
+        }
+        printf("\n\n");
+    }
+}
--- sort/QuickSort.c
+++ sort/QuickSort.c
@@ -28,6 +28,26 @@ void QuickSort(int arr[], int s, int t) {
     QuickSort(arr, s, k - 1);
     QuickSort(arr, k + 1, t);
 }
+void QuickSort2(int arr[], int s, int t) {
+    if (s >= t)
+        return;
+    int i = s, k = t + 1;
+    while (1) {
+        do
+            i++;
+        while (arr[i] < arr[s] && i <= k);
+        do
+            k--;
+        while (arr[k] > arr[s] && i <= k);
+        if (i < k)
+            Swap(arr + i, arr + k);
+        else
+            break;
+    }
+    Swap(arr + s, arr + k);
+    QuickSort(arr, s, k - 1);
+    QuickSort(arr, k + 1, t);
+}
 int main(int argc, char const *argv[]) {
     srand(time(NULL));
     int i, t = 6;
@@ -38,6 +58,12 @@ int main(int argc, char const *argv[]) {
         }
         puts("\n快速排序 --------------------");
         QuickSort(K, 0, M - 1);
+
+        for (i = 0; i < M; i++) {
+            printf("%d, ", K[i]);
+        }
+        puts("\n快速排序2 --------------------");
+        QuickSort2(K, 0, M - 1);
 
         for (i = 0; i < M; i++) {
             printf("%d, ", K[i]);
