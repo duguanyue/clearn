1affed0e4f573f3197a3caa26f21f313d4ba819f
duguanyue <duguanyue@gmail.com>
2018-10-09 15:45:34 +0800

    hash -2

--- hash/HashList.c
+++ hash/HashList.c
@@ -1,10 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MaxLen 100
-typedef int HashList[MaxLen];
-int Hash(int value) { return value % MaxLen; }
-int Insert(HashList list, int value) {
-    int i, D;
-    i = Hash(value);
+#include <time.h>
+#define MaxLen 20
+typedef struct node {
+    int data;
+} Node, *Entity;
+typedef Entity HashList[MaxLen];
+int Hash(Entity entity) { return entity->data % MaxLen; }
+int cmp(Entity e1, Entity e2) {
+    if (e1 == NULL || e2 == NULL)
+        return e1 == e2;
+    return e1->data == e2->data;
 }
-int main(int argc, char const *argv[]) { return 0; }
+int Insert(HashList list, Entity entity) {
+    if (entity == NULL)
+        return -1;
+    int i, D;
+    i = D = Hash(entity);
+
+    while (list[D] != NULL && !cmp(list[D], entity)) {
+        D = (D + 1) % MaxLen;
+        if (D == i)
+            return -1;
+    }
+    if (!cmp(list[D], entity))
+        list[D] = entity;
+
+    return D;
+}
+int main(int argc, char const *argv[]) {
+    HashList list;
+    Entity e;
+    srand(time(NULL));
+    int i, c, f;
+    for (i = 0; i < MaxLen; i++) {
+        list[i] = NULL;
+    }
+    for (i = 0; i < 10; i++) {
+        c = rand() % 100;
+        e = (Entity)malloc(sizeof(Node));
+        e->data = c;
+        f = Insert(list, e);
+        printf("%d insert number (%d) result is %d\n", i, c, f);
+    }
+
+    puts("显示插入结果:");
+    for (i = 0; i < MaxLen; i++) {
+        e = list[i];
+        if (e == NULL) {
+            printf("%d is Null\n", i);
+        } else {
+            printf("%d is %d\n", i, e->data);
+        }
+    }
+
+    return 0;
+}
