0c7c82457c1e637ca2f03534d8bd275986d2d313
duguanyue <duguanyue@gmail.com>
2018-11-01 17:22:01 +0800

    test 6-2

--- test/test6.c
+++ test/test6.c
@@ -6,7 +6,14 @@ typedef struct tree {
     struct tree *lChild, *rChild;
 } Node, *Tree;
 Tree STACK[M];
-Tree toTree(const char *str) {
+void Visit(Tree t) {
+    // print
+    if (t == NULL)
+        printf("NULL");
+    else
+        printf("%c, ", t->data);
+}
+Tree ToTree(const char *str) {
     Tree t = NULL, p = NULL;
     int flag, i = 0, top = -1;
     char ch;
@@ -42,17 +49,87 @@ Tree toTree(const char *str) {
     return NULL;
 }
 
+/**
+ * 中序遍历
+ */
 void LDR(Tree t) {
     if (t) {
         LDR(t->lChild);
-        printf("%c ", t->data);
+        Visit(t);
         LDR(t->rChild);
     }
 }
+/**
+ * 非递归中序遍历
+ */
+void NoReLDR(Tree t) {
+    Tree p = t;
+    int top = -1;
+    while (p || top != -1) {
+        while (p) {
+            STACK[++top] = p;
+            p = p->lChild;
+        }
+        p = STACK[top--];
+        Visit(p);
+        p = p->rChild;
+    }
+}
+/**
+ * 前序遍历
+ */
+void DLR(Tree t) {
+    if (t) {
+        Visit(t);
+        DLR(t->lChild);
+        DLR(t->rChild);
+    }
+}
+/**
+ * 非递归前序遍历
+ */
+void NoReDLR(Tree t) {
+    Tree p = t;
+    int top = -1;
+    while (p || top != -1) {
+        while (p) {
+            Visit(p);
+            STACK[++top] = p;
+            p = p->lChild;
+        }
+        p = STACK[top--];
+        p = p->rChild;
+    }
+}
+/**
+ * 后序遍历
+ */
+void LRD(Tree t) {
+    if (t) {
+        LRD(t->lChild);
+        LRD(t->rChild);
+        Visit(t);
+    }
+}
+
+void NoReLRD(Tree t) {
+    // TODO
+}
 
 int main(int argc, char const *argv[]) {
-    const char *str = "A(B(C,D(E(F,G),H(I))))@";
-    Tree t = toTree(str);
+    const char *str = "A(B(D(G,H),E),C(F(I)))@";
+    Tree t = ToTree(str);
+    puts("\n前序遍历");
+    DLR(t);
+    puts("\n前序遍历非递归");
+    NoReDLR(t);
+    puts("\n中序遍历");
     LDR(t);
+    puts("\n中序遍历非递归");
+    NoReLDR(t);
+    puts("\n后序遍历");
+    LRD(t);
+    puts("\n后序遍历非递归");
+    // NoReLRD(t);
     return 0;
 }
